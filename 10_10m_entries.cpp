81e5a3609ee0534846f68c89cab5ef7e5a0e9388
Mihir Bhasin <57122515+Mihir-Bhasin@users.noreply.github.com>
2022-01-03 14:31:33 +0530

    Create 10_10m_entries.cpp

---
+++ 10_10m_entries.cpp
@@ -0,0 +1,14 @@
+int findMinMax(int X[], int n)
+{
+    int max = X[0]
+    int min = X[0]
+    for (int i = 1; i &lt; n; i = i + 1)
+    {
+        if (X[i] &gt; max)
+            max = X[i]
+        else if (X[i] &lt; min)
+            min = X[i]
+    }
+    int res[2] = {max, min}
+    return res
+}
