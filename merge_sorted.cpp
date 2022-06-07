88b13292cc1d2d39a90636d24bfdd412c56abea6
Mihir Bhasin <57122515+Mihir-Bhasin@users.noreply.github.com>
2022-06-07 17:00:55 +0530

    Update merge_sorted.cpp

--- merge_sorted.cpp
+++ merge_sorted.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //merge with till value
+    //merge with till value looping 
     void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
            int i=0,j=0;
         vector<int> v;
